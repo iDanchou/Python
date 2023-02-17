2353a7079a398d4dc14c5426a029c1444890ead9
iDanchou <rkirkland18@live.com>
2023-02-17 15:31:04 -0500

    updates

--- Personal Projects/CSV and Pandas/main.py
+++ Personal Projects/CSV and Pandas/main.py
@@ -1,3 +1,21 @@
-with open("weather_data.csv") as data_file:
-    data = data_file.readlines()
-    print(data)
+# with open("Personal Projects\CSV and Pandas\weather_data.csv") as data_file:
+#     data = data_file.readlines()
+#     print(data)
+
+
+# import csv
+
+# with open("Personal Projects\CSV and Pandas\weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     print(data)
+#     for row in data:
+#         # print(row)
+#         # print(row[1])  # WILL PRINT EVERYTHING WITHIN THE TEMPERATURE ROW
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#     print(temperatures)
+
+import pandas
+
+pandas.read_csv(weather_data.csv)
